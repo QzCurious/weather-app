ec40439092505bb1a038fbe8b737ff0fb56e8e2f
Qz <qzcurious@gmail.com>
2022-01-29 02:25:53 +0800

    CSS Baseline
    https://mui.com/components/css-baseline/

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,9 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { CssBaseline } from "@mui/material";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -13,6 +13,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>{process.env.NEXT_PUBLIC_SITE_TITLE}</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
+      <CssBaseline />
       <Component {...pageProps} />
     </>
   );
