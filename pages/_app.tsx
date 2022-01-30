50ba11f106436909d4adb9f4a06a8ce9a394a5c3
Qz <qzcurious@gmail.com>
2022-01-30 14:58:13 +0800

    Integrate react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,17 +5,24 @@ import "@fontsource/roboto/700.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { CssBaseline } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { retry: false, refetchOnWindowFocus: false },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Head>
         <title>{process.env.NEXT_PUBLIC_SITE_TITLE}</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <CssBaseline />
       <Component {...pageProps} />
-    </>
+    </QueryClientProvider>
   );
 }
 
